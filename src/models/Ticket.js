b0abb1f3bcfbe5f5d76149e1da92f07ac33901da
webgaurav02 <gauravcodes123@gmail.com>
2025-03-19 15:04:34 +0530

    Update Ticket Modal

--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,13 +1,22 @@
+// models/Ticket.js
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ticketSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  transactionId: { type: String, required: true },
+const TicketSchema = new Schema({
+  bookingId: { type: Schema.Types.ObjectId, ref: 'Booking', required: true },
+  ticketNumber: { type: String, required: true },
+  userId: { type: String },
+  generatedAt: { type: Date, default: Date.now },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  public_id: {
+    type: String,
+    required: true,
+  },
+  emailSent: { type: Boolean, default: false },
   used: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Ticket', ticketSchema);
+module.exports = mongoose.model('Ticket', TicketSchema);
